798b3f5d0ae8618206f9720d861ec2e9f43743cd
Natan <natan-moreira1@hotmail.com>
2023-02-25 12:00:39 -0300

    add responsiveness on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+
 import logo from "../../assets/logo.svg";
 
 interface Props {
@@ -6,16 +9,31 @@ interface Props {
 }
 
 export const Header = ({ triggerUploadPhoto }: Props) => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const setResizeWidth = (e: any) => {
+      setWidth(e.target.innerWidth);
+    };
+    window.addEventListener("resize", setResizeWidth);
+
+    return () => window.removeEventListener("resize", setResizeWidth);
+  }, []);
+
   return (
     <div className="w-full bg-secondary/80">
-      <div className="mx-auto py-6 max-w-5xl flex items-center justify-between">
+      <div className="mx-5 lg:mx-auto py-6 max-w-5xl flex items-center justify-between">
         <Image src={logo} alt="upfi_logo" />
         <button
           aria-label="adicionar imagem"
           className="px-4 py-[10px] text-white text-md bg-orange hover:bg-orange/90 transition-colors font-bold rounded-[6px]"
           onClick={triggerUploadPhoto}
         >
-          Adicionar Imagem
+          {width > 400 ? (
+            "Adicionar Imagem"
+          ) : (
+            <AiOutlineCloudUpload size={20} />
+          )}
         </button>
       </div>
     </div>
